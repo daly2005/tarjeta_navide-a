2f3873af29e0a105808a7347d39ab063df8122f7
daly <daly08081998@gmail.com>
2024-12-09 10:33:47 -0400

    adding

---
+++ script00.js
@@ -0,0 +1,8 @@
+function toggleCard() {
+    const content = document.querySelector('.card-content');
+    if (content.style.display === 'none' || content.style.display === '') {
+        content.style.display = 'block';
+    } else {
+        content.style.display = 'none';
+    }
+}
