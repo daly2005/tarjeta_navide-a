c6540592b3bf5d5fb8823cba067cad2e6c331105
daly <daly08081998@gmail.com>
2024-12-10 00:04:32 -0400

    adding

--- script00.js
+++
@@ -1,8 +0,0 @@
-function toggleCard() {
-    const content = document.querySelector('.card-content');
-    if (content.style.display === 'none' || content.style.display === '') {
-        content.style.display = 'block';
-    } else {
-        content.style.display = 'none';
-    }
-}
